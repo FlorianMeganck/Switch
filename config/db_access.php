147bb6b05ec6608d30dec9efaddc5cd91c7a3ae5
Florian Megnck <meganckflorian@gmail.com>
2026-03-10 17:24:02 +0100

    access db

--- config/db_access.php
+++ config/db_access.php
@@ -1,5 +1,18 @@
 <?php
-define('USER','');
-define('PASSWD','');
-define('SERVER',''); // à demander à votre hébergeur
-define('BASE','');
+define('USER', 'hh76ic64hcii');
+define('PASSWD', 't7=h1h#4hh');
+define('SERVER', 'localhost');
+define('BASE', 'ebus2_projet05_siii57');
+
+// le seul endroit où je vais dire le type de base de données SQL que j'utilise
+$dsn = 'mysql:host=' . SERVER . ';dbname=' . BASE;
+
+try {
+    // créer la connexion  
+    $connexion = new PDO($dsn, USER, PASSWD);
+} catch (PDOException $e) {
+    echo 'Échec de la connexion : ' . $e->getMessage();
+    exit();
+}
+
+// si tout se passe bien alors, on a à disposition la variable $connexion
