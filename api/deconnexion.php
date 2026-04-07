6a0b94ddcdc0beee4c24575933cca67eec6cb4d6
Florian Megnck <meganckflorian@gmail.com>
2026-04-07 15:18:36 +0200

    destruction cookie deconnexion

--- api/deconnexion.php
+++ api/deconnexion.php
@@ -5,6 +5,18 @@ $_SESSION = array();
 
 session_destroy();
 
+if ($_GET['action'] == 'logout') {
+
+    if (ini_get('session.use_cookies')) {
+       $params = session_get_cookie_params();
+       setcookie(
+         session_name(),
+         '',
+         time() - 42000,
+         $params['path']
+       );
+     }
+
 echo json_encode(['success' => true]);
 exit;
 ?>
